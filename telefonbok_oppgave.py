5025189b1e1d2a122fe8fb1467bc1161f10e897f
200IQLulu <ludvigmegeberg@gmail.com>
2025-09-12 14:45:41 +0200

    Lagde en telefonbok liste og lagde to persjoner i en ord bok og puttet dem inni den første

---
+++ telefonbok_oppgave.py
@@ -0,0 +1,16 @@
+#oppgave 1
+
+telefonbok = []
+
+Persjon1 = {
+    "navn": "Ola",
+    "nummer": 18164173
+}
+
+Persjon2 = {
+    "navn": "Kari",
+    "nummer": 19045254
+}
+
+telefonbok.append(Persjon1)
+telefonbok.append(Persjon2)
